d9fd800dec555696cdb136c80df8fb165fd93a5e
ngarawakimani <dancankimani70@gmail.com>
2017-09-03 02:07:57 +0300

    changed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    wa kimani
                 </div>
 
                 <div class="links">
